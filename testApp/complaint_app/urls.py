dfe5e657407c3def3e07c942bcc9f1577183886a
vmcolon <>
2023-03-22 22:09:11 -0400

    updating urls.py

--- testApp/complaint_app/urls.py
+++ testApp/complaint_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from .views import ComplaintViewSet, OpenCasesViewSet, ClosedCasesViewSet, TopComplaintTypeViewSet
+from .views import ComplaintViewSet, OpenCasesViewSet, ClosedCasesViewSet, TopComplaintTypeViewSet, ConstituentComplaintViewSet
 
 router = routers.SimpleRouter()
 router.register(r'', ComplaintViewSet, base_name='complaint')
@@ -9,5 +9,11 @@ router.register(r'closedCases', ClosedCasesViewSet, base_name='closedCases')
 router.register(r'topComplaints', TopComplaintTypeViewSet, base_name='topComplaints')
 router.register(r'constituentsComplaints', ConstituentComplaintViewSet, base_name='constituentsComplaints')
 urlpatterns = [
+
+    path('openCases/', OpenCasesViewSet.as_view({'get': 'list'}), name='openCases'),
+    path('closedCases/', ClosedCasesViewSet.as_view({'get': 'list'}), name='closedCases'),
+    path('topComplaints/', TopComplaintTypeViewSet.as_view({'get': 'list'}), name='topComplaints'),
+    path('constituentsComplaints/', ConstituentComplaintViewSet.as_view({'get': 'list'}), name='constituentsComplaints'),
 ]
+
 urlpatterns += router.urls
